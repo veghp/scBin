ddcdd5168e1b9b2a8e1f456928546b3606d0c11f
veghp <31794642+veghp@users.noreply.github.com>
2019-05-30 11:30:13 +0100

    Copyright notice and license notice

--- R/scPlot.R
+++ R/scPlot.R
@@ -1,3 +1,21 @@
+# Copyright 2019 Peter Vegh
+# 
+# This file is part of scBin.
+# 
+# scBin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# scBin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with scBin.  If not, see <https://www.gnu.org/licenses/>.
+
+
 scPlot <- function(x, meta, markers, tnr = 0.95, jitter = F) {
 
   if(any(x < 0)) {stop("Negative values in count matrix.")}
